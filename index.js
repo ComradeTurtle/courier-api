a07d094928b55e421bf65cbfdf100f19ff3941de
ComradeTurtle <42468982+ComradeTurtle@users.noreply.github.com>
2021-03-20 19:47:56 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-class Elta {
-    get() {
-        return {
-            name: "elta"
-        }
+const axios = require("axios");
+
+class elta {
+    get(tr) {
+        return new Promise((resolve, reject) => {
+            axios.post("https://www.elta-courier.gr/track.php", `number=${tr}`)
+                .then((results) => {
+                    const data = results.data;
+                    let array = data.result[tr].result;
+
+                    if (!(data.status === 1)) reject(null);
+                    if (data.result[tr].status === 2) reject(null);
+                    if (array === "wrong number") reject('Invalid tracking number');
+
+                    resolve(array);
+                }).catch((err) => {
+                    reject(err);
+                });
+        });
     }
 }
 
-module.exports = { Elta };
+module.exports = { elta };
