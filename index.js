2f615415c02a4ee037a239a64bffec58b53433d2
ComradeTurtle <42468982+ComradeTurtle@users.noreply.github.com>
2021-03-20 23:06:32 +0200

    Added tracking for Geniki Taxydromiki

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const jsdom = require("jsdom");
 
 class elta {
     get(tr) {
@@ -20,4 +21,37 @@ class elta {
     }
 }
 
-module.exports = { elta };
+class geniki {
+    get(tr) { 
+        return new Promise(async (resolve, reject) => {
+            const { JSDOM } = jsdom;
+            const data = await axios.get(`https://www.taxydromiki.com/track/${tr}`);
+            const dom = new JSDOM(data.data, {  url: 'https://www.taxydromiki.com' });    
+            const trDOM = new JSDOM(dom.window.document.getElementById('edit-content').innerHTML);
+            let number = 0;
+            let obj = 0;
+            let trData = {};
+
+            Array.prototype.forEach.call(trDOM.window.document.getElementsByTagName('div'), (e) => {
+        
+                //! All credits due to @Tougrel (https://github.com/tougrel)
+                if(e.innerHTML.startsWith('<div')) return;
+                
+                if(!trData[obj]) trData[obj] = {};
+                if(e.innerHTML.includes('Κατάσταση')) trData[obj].status = e.innerHTML.substring(26);
+                if(e.innerHTML.includes('Τοποθεσία')) trData[obj].place = e.innerHTML.substring(26);
+                if(e.innerHTML.includes('Ημερομηνία')) trData[obj].date = e.innerHTML.substring(27);
+                if(e.innerHTML.includes('Ώρα')) trData[obj].time = e.innerHTML.substring(20);
+        
+                number += 1;
+                if(number == 4) {
+                    number = 0;
+                    obj += 1;
+                }
+            })
+            if(Object.keys(trData).length <= 1) reject({"status": "no result"});
+            resolve(trData);
+        })
+    }
+}
+module.exports = { elta, geniki };
