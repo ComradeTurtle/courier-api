88e803643297857f8916a72a4e75c424c4920a54
ComradeTurtle <giorgosd1300@shadowct.eu>
2021-03-30 21:39:16 +0300

    Output format changes & bug fixes
    - Classes output tracking data as array and not as an object.

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ class elta {
                     const data = results.data;
                     let array = data.result[tr].result;
 
-                    if (!(data.status === 1)) reject({"status": "no result"});
-                    if (data.result[tr].status === 2) reject({"status": "no result"});
-                    if (array === "wrong number") reject({"status": "no result"});
+                    if (!(data.status === 1)) reject({ "status": "no result" });
+                    if (data.result[tr].status === 2) reject({ "status": "no result" });
+                    if (array === "wrong number") reject({ "status": "no result" });
 
                     resolve(array);
                 }).catch((err) => {
@@ -22,46 +22,48 @@ class elta {
 }
 
 class geniki {
-    get(tr) { 
-        return new Promise(async (resolve, reject) => {
+    get(tr) {
+        return new Promise(async(resolve, reject) => {
             const { JSDOM } = jsdom;
             const data = await axios.get(`https://www.taxydromiki.com/track/${tr}`);
-            const dom = new JSDOM(data.data, {  url: 'https://www.taxydromiki.com' });    
+            const dom = new JSDOM(data.data, { url: 'https://www.taxydromiki.com' });
             const trDOM = new JSDOM(dom.window.document.getElementById('edit-content').innerHTML);
             let number = 0;
             let obj = 0;
             let trData = {};
+            let trArray = [];
 
             Array.prototype.forEach.call(trDOM.window.document.getElementsByTagName('div'), (e) => {
-        
+
                 //! All credits due to @Tougrel (https://github.com/tougrel)
-                if(e.innerHTML.startsWith('<div')) return;
-                
-                if(!trData[obj]) trData[obj] = {};
-                if(e.innerHTML.includes('Κατάσταση')) trData[obj].status = e.innerHTML.substring(26);
-                if(e.innerHTML.includes('Τοποθεσία')) trData[obj].place = e.innerHTML.substring(26);
-                if(e.innerHTML.includes('Ημερομηνία')) trData[obj].date = e.innerHTML.substring(27);
-                if(e.innerHTML.includes('Ώρα')) trData[obj].time = e.innerHTML.substring(20);
-        
+                if (e.innerHTML.startsWith('<div')) return;
+
+                if (!trData[obj]) trData[obj] = {};
+                if (e.innerHTML.includes('Κατάσταση')) trData[obj].status = e.innerHTML.substring(26);
+                if (e.innerHTML.includes('Τοποθεσία')) trData[obj].place = e.innerHTML.substring(26);
+                if (e.innerHTML.includes('Ημερομηνία')) trData[obj].date = (e.innerHTML.substring(27).split(', ')[1]);
+                if (e.innerHTML.includes('Ώρα')) trData[obj].time = e.innerHTML.substring(20);
+
                 number += 1;
-                if(number == 4) {
+                if (number == 4) {
                     number = 0;
                     obj += 1;
                 }
             })
-            if(Object.keys(trData).length <= 1) reject({"status": "no result"});
-            resolve(trData);
+            if (Object.keys(trData).length <= 1) reject({ "status": "no result" });
+            let array = Object.values(trData);
+            resolve(array);
         })
     }
 }
 
 class speedex {
     get(tr) {
-        return new Promise(async (resolve, reject) => {
+        return new Promise(async(resolve, reject) => {
             const { JSDOM } = jsdom;
             const data = await axios.get(`http://www.speedex.gr/speedex/NewTrackAndTrace.aspx?number=${tr}`);
-            if(data.data.includes('Δεν βρέθηκαν')) reject({"status": "no result"})
-            const dom = new JSDOM(data.data, {url: 'http://www.speedex.gr'});
+            if (data.data.includes('Δεν βρέθηκαν')) reject({ "status": "no result" })
+            const dom = new JSDOM(data.data, { url: 'http://www.speedex.gr' });
             let number = 0;
             let obj = 0;
             let trData = {};
@@ -78,7 +80,7 @@ class speedex {
 
             //! Get place, time and date
             Array.prototype.forEach.call(dom.window.document.getElementsByClassName('card-subtitle'), (e) => {
-                if(!trData[obj]) trData[obj] = {};
+                if (!trData[obj]) trData[obj] = {};
                 let desc = e.innerHTML.trim().split('>')[1];
 
                 trData[obj].status = stData[obj];
@@ -87,12 +89,13 @@ class speedex {
                 trData[obj].time = desc.substring(0, desc.length - 6).split(',')[1].split(' ')[3];
 
                 number += 1;
-                if(number == 1) {
+                if (number == 1) {
                     number = 0;
                     obj += 1;
                 }
             })
-            resolve(trData);
+            let array = Object.values(trData);
+            resolve(array);
         })
     }
 }
